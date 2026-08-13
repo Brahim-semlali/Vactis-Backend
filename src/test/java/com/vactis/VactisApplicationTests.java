20c710f4698d22824fdd5dbf2cb6a141176f76ff
Brahim Semlali <semlalibrahim34@gmail.com>
2026-08-13 21:03:19 +0100

    update properties test

--- src/test/java/com/vactis/VactisApplicationTests.java
+++ src/test/java/com/vactis/VactisApplicationTests.java
@@ -1,13 +1,17 @@
 package com.vactis;
 
+import com.vactis.config.DataFictifSeeder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class VactisApplicationTests {
 
+    @MockitoBean
+    private DataFictifSeeder dataFictifSeeder;
+
     @Test
     void contextLoads() {
     }
-
 }
